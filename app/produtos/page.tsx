14ca8e4a1e0fb4f9048b908d0da431f205773d38
miguelgouveia-lei <a22402506@alunos.ulht.pt>
2024-12-12 08:20:41 +0000

    final - no style

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React from 'react';
-import {Product} from '../app/models/interfaces'
+import {Product} from '../models/interfaces'
 import useSWR from 'swr';
 import Card from '@/components/Card/card'
 
 export default function Products() {
 
-    const fetcher = (url: string) => fetch(url).then(res => res.json())
-    const { data: products, error, isLoading } = useSWR<Product[], Error>('/api/products');
+    const fetcher = (url: string) => fetch(url).then(res => res.json()).catch(error => console.error('Erro:',error));
+    const { data: products, error, isLoading } = useSWR<Product[], Error>('api/products', fetcher);
     
     if (error) return <div>Failed to load</div>;
     if (isLoading) return <div>Loading...</div>;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
 
-export default function Header(){
-
-    return (
-
-        <header className = {styles.header}>
-        <nav className = {styles.nav}>
-
-        <Link href="/">Home</Link>
-        <Link href="/produtos">Products</Link>
-        <Link href="/tecnologias">Technology</Link>
-
-
-         </nav>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>React & Next.js</h1>
+      <nav className={styles.nav}>
+        <Link href="/">Home →</Link>
+        <Link href="/produtos">Produtos →</Link>
+        <Link href="/tecnologias">Tecnologias →</Link>
+      </nav>
+    </header>
+  );
 }
+
